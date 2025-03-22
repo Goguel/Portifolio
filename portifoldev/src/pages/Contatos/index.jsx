0850efd94d75628798190816756211eb7d37bf77
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-22 19:53:31 -0300

    feat: Adding contacts

--- portifoldev/src/pages/Contatos/index.jsx
+++ portifoldev/src/pages/Contatos/index.jsx
@@ -1,13 +1,33 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
+import styles  from './Contatos.module.css'
+import { FiMail } from "react-icons/fi";
+import { IoLogoGithub } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
 
 function Contatos() {
     return (
         <>
             <Header />
             <Container>
-                <h2>Contatos</h2>
+                <section className={styles.contatos}>
+                    <h2>Contatos</h2>
+                    <h3>Entre em contato</h3>
+                    <p>Para que possamos conversar mais sobre.</p>
+
+                    <div className={styles.icones}>
+                        <a href='mailto:miguelxmorais@gmail.com' target='_blank' rel='noopener noreferrer'>
+                        <FiMail className={styles.icone} />
+                        </a>
+                        <a href='https://www.github.com/Goguel' target='_blank' rel='noopener noreferrer'>
+                        <IoLogoGithub className={styles.icone} />
+                        </a>
+                        <a href='https://www.linkedin.com/in/miguel-xavier-de-morais/' target='_blank' rel='noopener noreferrer'>
+                        <FaLinkedin className={styles.icone} />
+                        </a>
+                    </div>
+                </section>
             </Container>
             <Footer />
             </>
