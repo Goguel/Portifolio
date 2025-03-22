2ee4e5dfee50f441225f1a72ef8faf7ef7c98aa4
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-22 18:34:54 -0300

    feat: Adding container to all pages

--- portifoldev/src/pages/Home/index.jsx
+++ portifoldev/src/pages/Home/index.jsx
@@ -1,26 +1,30 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
+import Container from '../../components/Container'
+import styles from './Home.module.css'
 
 function Home() {
     return (
         <>
             <Header />
-            <section className='container'>
-                <div className="apresentacao">
-                <p>
-                    Olá, sou <br/>
-                    <span>Miguel Xavier</span> <br/>
-                    Dev Full Stack
-                </p>
-                <Link to="/sobre" className="btn Info">
-                    Saiba mais
-                </Link>
-                </div>
-                <figure>
-                <img className="img-home" src="/feeling-proud-blue.svg" alt="Imagem da home" />
-                </figure>
-            </section>
+            <Container>
+                <section className={styles.home}>
+                    <div className={styles.apresentacao}>
+                    <p>
+                        Olá, sou <br/>
+                        <span>Miguel Xavier</span> <br/>
+                        Dev Full Stack
+                    </p>
+                    <Link to="/sobre" className={`${styles.btn} ${styles.btnInfo}`}>
+                        Saiba mais
+                    </Link>
+                    </div>
+                    <figure>
+                    <img className={styles.img_home} src="/feeling-proud-blue.svg" alt="Imagem da home" />
+                    </figure>
+                </section>
+            </Container>
             <Footer />
     </>
     )
