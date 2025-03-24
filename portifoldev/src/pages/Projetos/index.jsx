2b6496b53acbb1c285f9553bace0c27a37a77edb
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-24 18:44:05 -0300

    feat: Connecting to the Github API

--- portifoldev/src/pages/Projetos/index.jsx
+++ portifoldev/src/pages/Projetos/index.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react'
 import Card from '../../components/Card'
-
+import styles from './Projetos.module.css'
 function Projetos() {
-    return (
-        <>
-            <h2>Projetos</h2>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </>
 
+    const [ repositories, setRepositories ] = useState([])
+
+    useEffect(() => {
+        const buscarRepositorios = async () => {
+            const response = await fetch('https://api.github.com/users/goguel/repos')
+            const data = await response.json()
+            setRepositories(data)
+        }
+        buscarRepositorios()
+    }, [])
+
+    return (
+        <section className={styles.projetos}>
+            <h2>Projetos</h2>
+           {
+                repositories.length > 0 ? (
+                    <section className={styles.lista}>
+                       <Card />
+                    </section>
+                ) : (
+                    <p>Carregando repositórios...</p>
+                )
+           }
+        </section>
     )
 }
 
