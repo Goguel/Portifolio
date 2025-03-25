d29f6380ded221b39877d5d4b7f2ebdbe2eae903
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-25 20:47:15 -0300

    feat: Adding sandwich menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,36 @@
 import { Link } from 'react-router-dom'
 import styles from './header.module.css'
+import { HiMenu } from "react-icons/hi";
+import { useState } from 'react';
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
         <header className={styles.header}>
             <Link to="/">
             <span>miguelXavier.dev</span>
             </Link>
-            <nav>
+            <nav 
+                className={`${ styles.menuSandwich } ${ showMenu ? styles.show : ''}`}
+                onClick={toggleMenu}
+            >
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
+            <div 
+                className={styles.menuButton}
+                onClick={toggleMenu}
+            >
+                <HiMenu />
+            </div>
         </header>
     )
 }
