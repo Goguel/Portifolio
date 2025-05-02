33630db34f936a00b3178d3852349a861faca70a
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-05-02 12:06:16 -0300

    feat: Change to static card generation

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,63 +1,25 @@
-import { useEffect, useState } from 'react'
 import Card from '../../components/Card'
 import styles from './Projetos.module.css'
+import projetos from '../../data/projetos.js'
 
 function Projetos() {
-
-    const [ repositories, setRepositories ] = useState([]);
-
-    useEffect(() => {
-        const buscarRepositorios = async () => {
-            const responses = await Promise.all([
-                fetch('https://api.github.com/users/goguel/repos?page=1&per_page=50'),
-                fetch('https://api.github.com/users/goguel-s-cia/repos?page=1&per_page=50'),
-                fetch('https://api.github.com/repos/Null-bank/null_bank_backend')
-            ])
-
-            const data = await Promise.all(responses.map(res => res.json()));
-            const allRepos = [...data[0], ...data[1], data[2]];
-
-            const reposComLinguagem = await Promise.all(
-                allRepos.map(async (repo) => {
-                    const langResponse = await fetch(repo.languages_url);
-                    const languages = await langResponse.json();
-                    return { ...repo, languages};
-                })
-            );
-
-            setRepositories(reposComLinguagem)
-        };
-
-        buscarRepositorios();
-
-    }, []);
-
     return (
         <section className={styles.projetos}>
             <h2>Projetos</h2>
-           {
-            repositories.length > 0 ? (
-                <section className={styles.lista}>
-                    {
-                    repositories.map((repo) => 
-                        (
-                            <Card 
-                                key = {repo.id} 
-                                name = {repo.name}   
-                                description = {repo.description} 
-                                html_url = {repo.html_url} 
-                                languages = {repo.languages}
-                            />
-                        )
-                    )
-                    }
-                </section>
-            ) : (
-                <p>Carregando repositórios...</p>
-            )
-           }
+            <section className={styles.lista}>
+                {projetos.map((projeto) => (
+                    <Card 
+                        key={projeto.name}
+                        name={projeto.name}
+                        description={projeto.description}
+                        image={projeto.image}
+                        techs={projeto.techs}
+                        github={projeto.github}
+                    />
+                ))}
+            </section>
         </section>
-    )
+    );
 }
 
 export default Projetos
