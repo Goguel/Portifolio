5b83357402435852d1bd0ace29e090edeae6e483
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-05-02 12:12:23 -0300

    feat: Add first projects

---
+++ src/data/projetos.js
@@ -0,0 +1,41 @@
+const projetos = [
+  {
+    name: "StudyRats",
+    description: "Aplicação web para estímulo de estudos em grupo.",
+    image: "/projetosImgs/studyrats.png",
+    techs: ["Java", "Spring Boot", "Angular", "PostgreSQL", "Docker"],
+    github: "https://github.com/Goguel/StudyRats"
+  },
+  {
+    name: "EclipseHotel",
+    description: "API RESTful para gerenciamento de um hotel.",
+    image: "/projetosImgs/eclipsehotel.png",
+    techs: ["Java", "Spring Boot", "PostgreSQL"],
+    github: "https://github.com/Goguel/EclipseHotel"
+  },
+  {
+    name: "Passgen",
+    description: "Gerador de senhas aleatórias com interface via terminal.",
+    image: "/projetosImgs/passgen.png",
+    techs: ["C++"],
+    github: "https://github.com/Goguel/Passgen"
+  },
+    {
+      name: "Sloc",
+      description: "Contador de linhas de código, comentários e espaços em branco com interface via terminal.",
+      image: "/projetosImgs/sloc.png",
+      techs: ["C++"],
+      github: "https://github.com/Goguel/Sloc"
+    },
+    {
+      name: "Null Bank",
+      description: "Aplicação bancária fullstack com Spring Boot e Angular.",
+      image: "/projetosImgs/nullbank.png",
+      techs: ["Java", "Spring Boot", "Angular", "PostgreSQL", "Docker"],
+      github: "https://github.com/Null-bank/null_bank_backend"
+    },
+    
+  ];
+  
+  export default projetos;
+  
